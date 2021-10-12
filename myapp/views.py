1cb38d7c05245f699e02b2ffa14018813ef00f50
Jakmis <jakmis007@gmail.com>
2021-10-12 08:25:33 +0200

    gdhdfghd

--- myapp/views.py
+++ myapp/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("XXX Hello, world. You're at the polls index.")
+    return HttpResponse(" Hello, world. You're at the polls index.")
